7507240e382c3b68240e3676a749f71efba2fab4
BieLuk <lukk.bienkowski@gmail.com>
2023-08-20 22:23:24 +0200

    get book controller test

--- src/controller/book/bookController_test.go
+++ src/controller/book/bookController_test.go
@@ -1,6 +1,7 @@
 package book
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/BieLuk/library-backend/src/dto"
 	"github.com/BieLuk/library-backend/src/service/books"
@@ -8,6 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -25,6 +27,7 @@ func TestBookController_CreateBook(t *testing.T) {
 		Description: utils.Pointer("test description"),
 	}
 
+	// when
 	returnID := uuid.New()
 	bookServiceMock := books.NewMockBookService(t)
 	bookServiceMock.Mock.
@@ -37,12 +40,49 @@ func TestBookController_CreateBook(t *testing.T) {
 	}
 	utils.MockJsonPost(c, requestObject)
 
-	// when
+	// then
 	bookController := NewBookController(bookServiceMock)
 	bookController.CreateBook(c)
 
-	// then
 	response := fmt.Sprintf("{\"id\":\"%s\"}", returnID.String())
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, response, w.Body.String())
+}
+
+func TestBookController_GetBook(t *testing.T) {
+	// given
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	bookID := uuid.New()
+	c.Params = gin.Params{
+		{
+			Key:   "id",
+			Value: bookID.String(),
+		},
+	}
+	responseObject := dto.GetBookResponse{
+		ID:          bookID,
+		Name:        "Test book name",
+		Author:      "Test author name",
+		ISBN:        "1234567890123",
+		Description: utils.Pointer("test description"),
+	}
+
+	// when
+	bookServiceMock := books.NewMockBookService(t)
+	bookServiceMock.Mock.
+		On("GetBook", bookID).
+		Return(&responseObject, nil).
+		Once()
+
+	// then
+	bookController := NewBookController(bookServiceMock)
+	bookController.GetBook(c)
+
+	responseJson, err := json.Marshal(responseObject)
+	require.NoError(t, err)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, string(responseJson), w.Body.String())
 }
